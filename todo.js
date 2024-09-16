4dd81303f5e80fb2d93ba5b4ebe73cebec3e6e6b
Mikee55 <milkyasterefe@gmail.com>
2024-09-16 11:40:47 +0300

    All commit
    Some

---
+++ todo.js
@@ -0,0 +1,25 @@
+const taskInput = document.getElementById('taskInput');
+const button = document.getElementById('button');
+const taskList = document.getElementById('task-list');
+
+button.addEventListener('click', function (e) {
+    e.preventDefault();
+    
+    const newListItem = document.createElement('li');
+    const checkBox = document.createElement('input');
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';  
+    checkBox.type = 'checkbox';
+    
+    newListItem.append(taskInput.value);
+    newListItem.append(checkBox);
+    newListItem.append(removeButton);
+    taskList.append(newListItem);  
+
+    
+    // const remButton = document.querySelector('button');
+    removeButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        taskList.removeChild(newListItem);
+    })
+});
