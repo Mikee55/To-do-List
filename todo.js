89cdb687822b70b16b1b8cae82c355da5b3439d4
Mikee55 <milkyasterefe@gmail.com>
2024-09-17 15:52:18 +0300

    Placeholder change

--- todo.js
+++ todo.js
@@ -5,17 +5,17 @@ const taskList = document.getElementById('task-list');
 button.addEventListener('click', function (e) {
     e.preventDefault();
 
-    if () {
-        
-    }
-    
+    const taskText = taskInput.value;
     const newListItem = document.createElement('li');
     const checkBox = document.createElement('input');
     const removeButton = document.createElement('button');
     removeButton.textContent = 'Remove';  
     checkBox.type = 'checkbox';
     
-    newListItem.append(taskInput.value);
+    if (taskText === "") {
+        
+    }
+    newListItem.append(taskText);
     newListItem.append(checkBox);
     newListItem.append(removeButton);
     taskList.append(newListItem);  
