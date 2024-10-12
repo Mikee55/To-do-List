650ad9e3f090f509f707c52884da8e14fea3d63a
Mikee55 <milkyasterefe@gmail.com>
2024-10-12 15:18:17 +0300

    update 2

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('hello Mike');
+let num = 0;
+
+let timer = setInterval(function(){
+    num +=3;
+    console.log(num);
+    if (num > 10) {
+        clearInterval(timer);
+    }
+}, 2000);
