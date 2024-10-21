d400060c7a61a2897e80a4909f3f4c33de4bdb28
Mikee55 <milkyasterefe@gmail.com>
2024-10-21 11:05:30 +0300

    update 14

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express');
-
+const ejs = require('ejs');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(express.static(__dirname + '/public'));
+
+var data = [{item: 'Mike'}, {item: 'SE'}, {item: '30'}];
+
 app.get('/', (req,res)=>{
     console.log('Home Page');
-    res.sendFile(__dirname + '/todo.html');
+    res.render('todo', { todos: data });
 })
+// app.post('/add-todo', (req,res) => {
+//     const newToDo = {text: req.body.todo};
+//     todos.push(newToDo);
+//     res.redirect('/');
+// });
+
+
 
 const port = 3000;
 app.listen(port, () => {
