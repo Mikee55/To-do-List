a603bbdde6b1df5a30d7c1bb6009171ff2b46e41
Mikee55 <milkyasterefe@gmail.com>
2024-11-01 22:27:45 +0300

    update 22

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.delete('/todo/:id', async (req,res) =>{
 
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}...`);
 });
